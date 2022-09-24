6837f231d39fe9fad20dd435d5aae3e3d88953b9
Robin <robin@norrland.xyz>
2022-09-24 13:38:46 +0200

    started matrix integration

--- SKYDD/src/main.rs
+++ SKYDD/src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+//Matrix någonting?
+use std::{convert::TryFrom, str::SplitAsciiWhitespace};
+use matrix_sdk::{
+    Client, config::SyncSettings,
+    ruma::{user_id, events::room::message::SyncRoomMessageEvent},
+};
+
+//iced (gui) imports
+use iced::{executor, Application, Command, Element, Settings, Text};
+
+//main
+#[tokio::main]
+async fn main() {
+    matrixtest().await;
+    icedtest();
+}
+
+fn icedtest() -> iced::Result  {
+    Hello::run(Settings::default())
+}
+
+//Shows text
+struct Hello;
+
+impl Application for Hello {
+    type Executor = executor::Default;
+    type Message = ();
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
+        (Hello, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool application")
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
+}
+
+async fn matrixtest() -> anyhow::Result<()> {
+	let userid = user_id!("@testuser3:norrland.xyz");
+	let client = Client::builder().user_id(userid).build().await?;
+
+    // First we need to log in.
+    client.login(userid, "yahoogimmickchamberhypnoticechounfoundedbonedunpainted", None, None).await?;
+
+	client.register_event_handler(|ev: SyncRoomMessageEvent| async move {
+		println!("Received a message {:?}", ev);
+	})
+	.await;
+
+    // Syncing is important to synchronize the client state with the server.
+    // This method will never return.
+    client.sync(SyncSettings::default()).await;
+
+    Ok(())	
 }
